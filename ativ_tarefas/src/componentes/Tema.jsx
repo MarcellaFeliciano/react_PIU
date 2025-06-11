25d1b20e3bd7ebbee56e607650aa2ccaf2988346
Marcella Feliciano <153116960+MarcellaFeliciano@users.noreply.github.com>
2025-06-11 16:16:01 -0300

    add

--- ativ_tarefas/src/componentes/Tema.jsx
+++ ativ_tarefas/src/componentes/Tema.jsx
@@ -8,6 +8,19 @@ export default function Tema({children}){
     // função que lida com a mudança de estado do tema, pegando uma função anonima, var auxiliar, para chamar a função do useState setTheme
     const toggleTheme = () => {setTheme((tema) => (tema === 'light'?'dark':'light'))}
 
+
+    const [cont,setCont] = useState(0)
+
+
+    let buttonColor = 'padrao'
+    if (cont >= 10) {
+        buttonColor = 'green';
+    } else if (cont < 0) {
+        buttonColor = 'red';
+    } else {
+        buttonColor = 'padrao';
+    }
+    
     return(
         // altera o nome da classe do CSS, mudando a cor de acordo com a variavel thema
         <div className={theme}>
@@ -18,6 +31,10 @@ export default function Tema({children}){
 
             {/* chama o componente dashboard (que é filho de tema) */}
            
+
+            <h4>Contador = {cont}</h4>
+           <button className={buttonColor} onClick={() => setCont((count) => count + 1)}>Acrescentar</button>
+           <button className={buttonColor} onClick={() => setCont((count) => count - 1)}>Diminuir</button>
            {children}
         </div>
 
